f4dc8cac3dba64069e5326626577c86ad996e02f
Paul Blair <paul@fairlieagile.co.nz>
2021-05-14 16:03:05 +1200

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fairlie-agile-components-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
