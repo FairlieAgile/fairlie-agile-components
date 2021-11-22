49b609a1aa4909dc5e068ac4304ee2e3ec08637c
Paul Blair <paul@fairlieagile.co.nz>
2021-11-23 11:38:01 +1300

    wip

--- apps/demo/src/environments/environment.ts
+++ apps/demo/src/environments/environment.ts
@@ -5,8 +5,8 @@ export const environment = {
   production: false,
   environment: 'dev',
   tenants: [
-    { tenantId: 'tenant1', friendlyName: 'tenant 1', urls: ['localhost', 'tenant1.localhost'] },
-    { tenantId: 'tenant2', friendlyName: 'tenant 2', urls: ['tenant2.localhost'] },
+    { tenantId: 'tenant1', friendlyName: 'tenant 1', clientId: '', urls: ['localhost', 'tenant1.localhost'] },
+    { tenantId: 'tenant2', friendlyName: 'tenant 2', clientId: '', urls: ['tenant2.localhost'] },
   ]
 }
 
