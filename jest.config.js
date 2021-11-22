ce31523317993d3500b2f812ce02e87616121e2a
Paul Blair <paul@fairlieagile.co.nz>
2021-11-23 11:44:11 +1300

    ng13

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/demo',
-    '<rootDir>/libs/ui/mat-incrementor',
-    '<rootDir>/libs/ui/material',
-    '<rootDir>/libs/ui/dev-banner',
-    '<rootDir>/libs/util/fa-tenant',
-    '<rootDir>/libs/util/fa-theme-picker',
-    '<rootDir>/libs/ui/layout',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
