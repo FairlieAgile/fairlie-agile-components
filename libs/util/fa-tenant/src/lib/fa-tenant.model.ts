1e37fc37791e1059778abd8fa5c85c5ff942bf0c
Paul Blair <paul@fairlieagile.co.nz>
2022-06-15 15:26:09 +1200

    change tenant fields

--- libs/util/fa-tenant/src/lib/fa-tenant.model.ts
+++ libs/util/fa-tenant/src/lib/fa-tenant.model.ts
@@ -1,9 +1,9 @@
 export interface FaTenant {
   tenantId: string,
   clientId: string,
-  friendlyName: string,
-  theme?: string,
-  favIcon?: string,
-  logo?: string,
+  displayName: string,
+  siteName: string,
+  termsText?: string,
+  excludeUrls: string[],
   urls: string[]
 }
